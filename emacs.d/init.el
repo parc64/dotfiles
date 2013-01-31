e4f41fb084de9a1369ec922d8dfc9e942309907a
Jason Vowell <jason@consensuspoint.com>
2013-01-31 08:53:29 -0600

    emacs config changes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,15 +5,16 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (adwaita)))
  '(inhibit-startup-screen t)
+ '(speedbar-directory-button-trim-method (quote trim))
+ '(speedbar-hide-button-brackets-flag nil)
+ '(speedbar-show-unknown-files t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-;; '(default ((t (:inherit nil :stipple nil :background "#EDEDED" :foreground "gray26" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
-
- '(default ((t (:inherit nil :stipple nil :background "#EDEDED" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+ '(default ((t (:slant normal :weight normal :height 120 :width normal :family "Droid Sans Mono")))))
 
 
 ;; ====================
