888d8794f4fb80ad36e91e40f1812c2263bffd97
Jason Vowell <jason@consensuspoint.com>
2013-02-01 12:05:40 -0600

    add emacs packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -14,7 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :height 120 :width normal :family "Droid Sans Mono")))))
+ '(default ((t (:slant normal :weight normal :height 160 :width normal :family "Droid Sans Mono")))))
 
 
 ;; ====================
