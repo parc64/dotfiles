12bc3f4e3aecb470d2e3eb1c44c498a06259f29c
Jason Vowell <jason@consensuspoint.com>
2013-02-11 07:26:35 -0600

    updates

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,13 +8,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenburn)))
+ '(ansi-color-names-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(ansi-term-color-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(custom-enabled-themes (quote (adwaita)))
  '(custom-safe-themes (quote ("0bac11bd6a3866c6dee5204f76908ec3bdef1e52f3c247d5ceca82860cccfa9d" default)))
+ '(fci-rule-color "#383838")
  '(fringe-mode (quote (nil . 0)) nil (fringe))
  '(inhibit-startup-screen t)
  '(show-paren-mode t)
  '(speedbar-directory-button-trim-method (quote trim))
- ;; '(speedbar-hide-button-brackets-flag nil)
  '(speedbar-show-unknown-files t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -28,9 +30,9 @@
 (require 'ido)
 (ido-mode t)
 
-(require 'helm)
-(global-set-key (kbd "C-c h") 'helm-mini)
-(helm-mode 1)
+;; (require 'helm)
+;; (global-set-key (kbd "C-c h") 'helm-mini)
+;; (helm-mode 1)
 
 (require 'scala-mode2)
 (require 'projectile)
@@ -42,10 +44,10 @@
 
 (require 'sr-speedbar)
 (global-set-key (kbd "s-s") 'sr-speedbar-toggle)
+(setq sr-speedbar-right-side 1)
 
 (require 'ecb-autoloads)
 
-(global-set-key (kbd "<f2>") 'sr-speedbar-toggle)
 (global-hl-line-mode 1)
 (setq standard-indent 2)
 (setq scroll-step 1)
